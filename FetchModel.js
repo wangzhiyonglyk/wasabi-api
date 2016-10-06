fb8daa9c765c8ca73b65193d274f0feda71be522
wangzhiyonglyk <wangzhiyonglyk@163.com>
2016-10-06 13:21:14 +0800

    modify

--- FetchModel.js
+++ FetchModel.js
@@ -7,12 +7,13 @@
 
 class FetchModel
 {
-    constructor(url,success,params=null,error=null,type="GET")
+    constructor(url,success,params=null,error=null,lang="java",type="GET")
     {
         this.url=url;
         this.params=params;
         this.success=success;
         this.error=error;
+        this.lang=lang;
         this.type=type;//类型,专门用于then
 
     }
